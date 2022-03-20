d7fa5ed42fdcf89659205a757a7d3e223965a53e
xiaozhi <z2376731805@gmail.com>
2022-03-20 17:51:19 +0800

    增加设置canvas dom 层级选项

--- src/L.CustomCanvas.js
+++ src/L.CustomCanvas.js
@@ -39,7 +39,7 @@ L.Canvas.CustomCanvas = L.Layer.extend({
     const canvas = L.DomUtil.create('canvas', 'leaflet-ship')
     const ctx = canvas.getContext('2d')
 
-    canvas.style.zIndex = '500'
+    canvas.style.zIndex = this.options.zIndex || '500'
 
     this._canvas = canvas
     this._ctx = ctx
