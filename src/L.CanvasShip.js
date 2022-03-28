3e5f07e9a0adda3405624e530979db686d862f44
xiaozhi <z2376731805@gmail.com>
2022-03-28 16:46:47 +0800

    增加事件注册功能

--- src/L.CanvasShip.js
+++ src/L.CanvasShip.js
@@ -1,3 +1,5 @@
+import lodash from "lodash";
+
 /**
  * 船舶图标绘制
  */
@@ -10,7 +12,7 @@ class CanvasShip extends L.Canvas.CustomCanvas {
       _map: map,
     } = this;
 
-    this._ctxs = []
+    this._ctxs = [];
 
     // 数据处理部分（临时代码）
     const res = latlngs.map((item) => {
@@ -23,34 +25,29 @@ class CanvasShip extends L.Canvas.CustomCanvas {
         latlng: map.latLngToContainerPoint(latlng),
         deg: item.deg || 0,
         type: item.type,
-        color : item.color
+        color: item.color,
       };
+
+      result.latlng.x *= 2;
+      result.latlng.y *= 2;
       return result;
     });
 
-    // 绘制图标时用到的 2倍坐标
-    const _ctxPoints = res.map( item => {
-      const res = {
-        ...item,
-        latlng : {
-          x : item.latlng.x * 2,
-          y : item.latlng.y * 2
-        }
-      }
-      return res
-    })
+    this._ctxPoints = res;
+    if (!Array.isArray(this._prevEvent)) this._prevEvent = [];
 
-    this._ctxPoints = _ctxPoints
-
-    this.drawShip(_ctxPoints);
-    this._initEvents()
+    this.drawShip(res);
+    this._initEvents();
   }
 
   /**
    * 画船
    * @param {Array} latlngs 船舶坐标点
+   * @param {Object} point 鼠标点击位置 非必填 已 * 2
+   * @param {Number} point.x 鼠标 x 位置
+   * @param {Number} point.y 鼠标 y 位置
    */
-  drawShip(latlngs) {
+  drawShip(latlngs, point) {
     const { _ctx: ctx } = this;
 
     // 船舶图标默认颜色
@@ -109,29 +106,29 @@ class CanvasShip extends L.Canvas.CustomCanvas {
       },
 
       // 信号延迟 > 6小时
-      lost(item){
-        this.base(item)
+      lost(item) {
+        this.base(item);
 
-        item.color = '#e9e9e9'
+        item.color = "#e9e9e9";
       },
 
       // 浮式存储
-      storage(item){
+      storage(item) {
         let { x, y } = item.latlng;
 
         // x = x * 2;
         // y = y * 2;
 
-        ctx.beginPath()
-        ctx.arc(x,y,18,0,2*Math.PI)
-        ctx.stroke()
-        ctx.moveTo(x + 10, y)
-        ctx.arc(x,y,11,0,2*Math.PI)
-        ctx.stroke()
+        ctx.beginPath();
+        ctx.arc(x, y, 18, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.moveTo(x + 10, y);
+        ctx.arc(x, y, 11, 0, 2 * Math.PI);
+        ctx.stroke();
         // ctx.strokeStyle="blue";//将线条颜色设置为蓝色
-        item.color = true
-          
-        return ctx
+        item.color = true;
+
+        return ctx;
       },
 
       end(item) {
@@ -139,56 +136,103 @@ class CanvasShip extends L.Canvas.CustomCanvas {
         ctx.fill(); // 以填充方式绘制 默认黑色
         ctx.stroke(); // 闭合形状并且以填充方式绘制出来
 
-        return ctx
+        return ctx;
       },
     };
 
+    let res = null;
     latlngs.forEach((item) => {
-      shapes[item.type](item);
+      let ctxItem = shapes[item.type](item);
+      /**
+       * 检测鼠标是否在形状哪
+       */
+      const inRectFn = (ctx) => {
+        return ctx.isPointInPath(point.x, point.y);
+      };
 
-      if(item.type === 'storage') {
-        const ctxItem = shapes[item.type](item);
-        this._ctxs.push(ctxItem)
-        return
+      if (item.type === "storage") {
+        if (point !== undefined) {
+          if (inRectFn(ctxItem)) {
+            res = point;
+          }
+        }
+        return;
       }
 
-      const ctxItem = shapes.end(item)
-      this._ctxs.push(ctxItem);
+      ctxItem = shapes.end(item);
+      if (point !== undefined) {
+        if (inRectFn(ctxItem)) {
+          res = point;
+        }
+      }
     });
-    
+
+    return res;
   }
 
   /**
-  * canvas 元素事件触发默认函数
-  */
- canvasDomEvent(callBack){
-   const { _ctxs ,_ctxPoints } = this
-   console.log(_ctxPoints, '_ctxPoints');
-  return callBack()
- }
+   * canvas 元素事件触发默认函数
+   * @param {Function} callBack 注册的事件函数
+   * @param {Object} event 出发事件坐标
+   */
+  canvasDomEvent(callBack, event) {
+    const { _ctxPoints, _canvas } = this;
+
+    let { x, y } = event;
+    x *= 2;
+    y *= 2;
+
+    const w = _canvas.width;
+    const h = _canvas.height;
+    _canvas.width = w;
+    _canvas.height = h;
+
+    const res = this.drawShip(_ctxPoints, { x, y });
+    if (res !== null) {
+      return callBack(res);
+    }
+  }
 
   /**
-  * 初始化元素事件
-  */
- _initEvents(){
-   const { _ctxs , _ctx:ctx , options:{events = {}} , _canvas} = this
+   * 初始化元素事件
+   */
+  _initEvents() {
+    const {
+      _ctxs,
+      _ctx: ctx,
+      options: { events = {} },
+      _canvas,
+    } = this;
 
-   const eventItems = Object.entries(events)
-   
-   if( eventItems.length === 0 ){
-    //  return 
-   }
+    const eventItems = Object.entries(events);
 
+    if (eventItems.length === 0) {
+      // return;
+    }
 
-   eventItems.forEach( item => {
-     const eventKey = item[0]
-     const eventHandle = item[1]
+    // 删除之前注册的事件
+    if (this._prevEvent.length !== 0) {
+      this._prevEvent.forEach((item) => {
+        _canvas.removeEventListener(item.key, item.fn);
+      });
 
-     console.log(eventKey, 'eventKey');
-     _canvas.removeEventListener(eventKey , this.canvasDomEvent)
-     _canvas.addEventListener(eventKey , this.canvasDomEvent(eventHandle))
-   })
- }
+      this._prevEvent = [];
+    }
+
+    eventItems.forEach((item) => {
+      const eventKey = item[0];
+      const eventHandle = item[1];
+
+      const currentEvent = lodash.debounce(
+        this.canvasDomEvent.bind(this, eventHandle),
+        150
+      );
+
+      this._prevEvent.push({ key: eventKey, fn: currentEvent });
+
+      _canvas.addEventListener(eventKey, currentEvent);
+    });
+  }
 }
 
 /**
@@ -197,6 +241,7 @@ class CanvasShip extends L.Canvas.CustomCanvas {
  * @param {Array} options.latlngs 经纬度数组 [{lat : 纬度, lng:经度, deg : 旋转角度 , color : 图标颜色}]
  * @param {Number | String} options.latlngs.deg 旋转角度 0-360
  * @param {String} options.latlngs.type base 基础形状 running 航行中 turn 转向 lost 信号延迟 > 6h storage 浮式存储
+ * @param {Object} events 要注册的事件 example : {click( point ){ xxx }}； point 代表鼠标坐标在 图标内时的 鼠标坐标 * 2
  * @return 创建的实例对象，将会是一个单例当重复创建时只会覆盖参数而不创建新的实例(暂未实现)
  */
 L.canvas.CanvasShip = (options) => {
